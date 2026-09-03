814439bbfeb295fbf443aabb07c477abe6202589
Personual <bavarite@gmail.com>
2026-09-03 20:14:56 +0300

    Update PlayerViewController.swift

--- Sources/UI/Player/PlayerViewController.swift
+++ Sources/UI/Player/PlayerViewController.swift
@@ -1,11 +1,8 @@
 import UIKit
-import WebKit
+import AVKit
 
-final class PlayerViewController: UIViewController, WKNavigationDelegate {
+final class PlayerViewController: AVPlayerViewController {
     private let url: URL?
-    private var webView: WKWebView!
-    private let progressView = UIProgressView(progressViewStyle: .default)
-    private var progressObservation: NSKeyValueObservation?
 
     init(url: URL?) {
         self.url = url
@@ -20,50 +17,10 @@ final class PlayerViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         view.backgroundColor = .black
 
-        let config = WKWebViewConfiguration()
-        config.allowsInlineMediaPlayback = true
-        config.mediaTypesRequiringUserActionForPlayback = []
-
-        webView = WKWebView(frame: .zero, configuration: config)
-        webView.navigationDelegate = self
-        webView.allowsBackForwardNavigationGestures = true
-        webView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(webView)
-
-        progressView.translatesAutoresizingMaskIntoConstraints = false
-        progressView.progressTintColor = .white
-        view.addSubview(progressView)
-
-        let backButton = GlassIconButton(systemImage: "chevron.backward")
-        backButton.addTarget(self, action: #selector(backTapped), for: .touchUpInside)
-        backButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(backButton)
-
-        NSLayoutConstraint.activate([
-            progressView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-
-            webView.topAnchor.constraint(equalTo: progressView.bottomAnchor),
-            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-
-            backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
-            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
-        ])
-
-        progressObservation = webView.observe(\.estimatedProgress, options: [.new]) { [weak self] webView, _ in
-            self?.progressView.progress = Float(webView.estimatedProgress)
-            self?.progressView.isHidden = webView.estimatedProgress >= 1.0
-        }
-
         if let url = url {
-            webView.load(URLRequest(url: url))
+            let player = AVPlayer(url: url)
+            self.player = player
+            player.play()
         }
-    }
-
-    @objc private func backTapped() {
-        navigationController?.popViewController(animated: true)
     }
 }
